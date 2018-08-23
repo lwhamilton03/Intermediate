7e3024b8bb1dea070a3fe1612fcdd315512b5ab6
lwhamilton03 <lucy.hamilton@academytrainee.com>
2018-08-23 12:04:59 +0100

    initialise

---
+++ src/TooHot.java
@@ -0,0 +1,16 @@
+
+public class TooHot {
+
+	public boolean tooHot(int temperature, boolean isSummer)
+	{
+		if (temperature <= 90 && temperature >= 60 && !isSummer)
+		{
+			return true; 
+		}
+		if (temperature >= 60 && isSummer)
+		{
+			return true;
+		}
+	return false;
+	}
+}
---
+++ src/UniqueSum.java
@@ -0,0 +1,26 @@
+
+public class UniqueSum {
+
+	public int uniqueSum(int a, int b, int c)
+	{
+		int result = 0 ;
+				
+		if (a != b && a != c)
+		{
+			result = result + a;
+		}
+		
+		if (b != a && b != c)
+		{
+			result = result + b;
+		}
+		
+		if (c != a && c != b)
+		{
+			result = result + c; 
+		}
+		
+		return result; 
+	}
+		
+}
